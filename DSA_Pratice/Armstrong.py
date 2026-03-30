205e30f0773e2eeca6aa2928b0b9122703c56d84
Buvashid9579 <prathameshchavan00319@gmail.com>
2026-03-30 13:09:16 +0530

    Added Armstrong problem

---
+++ DSA_Pratice/Armstrong.py
@@ -0,0 +1,13 @@
+n = 153
+Num = n
+total = 0 
+nod = len(str(n))
+while(Num > 0):
+    rem = Num % 10
+    total = total + rem ** nod
+    Num = Num // 10
+
+if(total == n):
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
